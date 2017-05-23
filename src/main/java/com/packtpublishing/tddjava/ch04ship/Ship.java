6cb3f2c1d2b007061c8504f022bd2450c144cf65
cgvelazquez <cesaringo55@gmail.com>
2017-05-22 23:41:43 -0400

    Add Instantiated, backward, forwdward test specificatons

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -1,5 +1,21 @@
 package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
-
+	private final Location location;
+	
+	public Ship( Location location ){
+		this.location = location;
+	}
+	
+	public Location getLocation(  ){
+		return this.location;
+	}
+	
+	public void moveForward(){
+		this.location.forward();
+	}
+	
+	public void moveBackward(){
+		this.location.backward();
+	}
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,33 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+	private Location location;
+	private Ship ship;
+	
+	@BeforeTest
+	public void beforeTest(){
+		location = new Location(
+				new Point(21,13), 
+				Direction.NORTH
+		);
+		ship = new Ship(location);
+	}
+
+	public void whenInstantiatedThenLocationIsSet(){
+		assertEquals(location, ship.getLocation());
+	}
+	
+	public void whenForwardThenForward(){
+		Location expected = this.location.copy();
+		expected.forward();
+		ship.moveForward();
+		assertEquals(ship.getLocation(),expected);
+	}
+	
+	public void whenBackwardThenBackward(){
+		Location expected = this.location.copy();
+		expected.backward();
+		ship.moveBackward();
+		assertEquals(ship.getLocation(),expected);
+	}
 }
